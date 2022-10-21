d2f6efdab094540333ff132034df157c416dfaa6
bradj00 <brjohnson00@gmail.com>
2022-10-21 00:08:44 -0500

    UI adds

--- web-frontend/src/components/P2AssetBox.jsx
+++ web-frontend/src/components/P2AssetBox.jsx
@@ -6,6 +6,7 @@ import WarningIcon from '@mui/icons-material/Warning';
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import TheUserErc20Balances from './sub-components/TheUserErc20Balances';
 import { useERC20Balances, useMoralis } from "react-moralis";
+import useWindowDimensions from './useWindowDimensions';
 
 const P2AssetBox = (props) => {
   const [anyP2Values, setanyP2Values] = useState();
@@ -14,6 +15,12 @@ const P2AssetBox = (props) => {
   const {displayUserErc20Assets, setdisplayUserErc20Assets} = useContext(generalContext);
   const {userErc20TokenBalance, setuserErc20TokenBalance}   = useContext(generalContext);
 
+  const { height, width } = useWindowDimensions(); //custom hook for window dimensions
+
+
+  useEffect(()=>{
+    console.log('new width:', width)
+  },[width]);
   return (
     <>
 
@@ -57,11 +64,11 @@ const P2AssetBox = (props) => {
             </div>
 
           </div>
-          <div style={{paddingLeft:'0.5vw', display:'flex', position:'absolute', bottom:'0', left:'0', height:'50%', width:'100%', backgroundColor:'rgba(255, 77, 38, 0.44)'}}>
+          <div style={{paddingLeft:'0.5vw', display:'flex', justifyContent:'center', position:'absolute', bottom:'0', left:'0', height:'50%', width:'100%', backgroundColor:'rgba(255, 77, 38, 0.44)'}}>
             <div style={{fontSize:'3vh'}}>
               ERC-721 Assets
             </div>  
-            <div style={{width:'80vw',height:'75%',top:'20%', position:'absolute', backgroundColor:'rgba(0,0,0,0.3)', border:'1px dashed #999'}}>
+            <div style={{width:'80vw',height:'75%',top:'20%', position:'absolute', left:'0.5vw', backgroundColor:'rgba(0,0,0,0.3)', border:'1px dashed #999'}}>
 
             <table style={{width:'100%'}} >
             <tbody>
@@ -73,7 +80,8 @@ const P2AssetBox = (props) => {
             </tr>
             <tr>
               <td>Bored Ape</td>
-              <td></td>
+              <td>
+              </td>
               <td>157</td>
               <td><a href="https://yahoo.com" target='blank'>https://..</a></td>
             </tr>
@@ -95,23 +103,25 @@ const P2AssetBox = (props) => {
             </div>
           </div>
 
-        {!props.counterParty?
-        <div style={{position:'absolute', right:'0.5vw',top:'7vh'}}>
-            <div style={{alignItems:'center',display:'flex',justifyContent:'center'}}>
+        {!props.counterParty?<></>
+        // tap the Table Id to get this instead....dont show it here 
+        // <div style={{position:'absolute', right:'0.5vw',top:'7vh'}}>
+        //     <div style={{alignItems:'center',display:'flex',justifyContent:'center'}}>
 
-            <div style={{border:'2px solid #fff', top:'10%',textAlign:'center',}}>
-              <QRCode size={185} value={UserActiveTable? baseUrl+UserActiveTable: "something went wrong!"} /> 
-            </div>
+        //     <div style={{border:'2px solid #fff', top:'10%',textAlign:'center',}}>
+        //       <QRCode size={185} value={UserActiveTable? baseUrl+UserActiveTable: "something went wrong!"} /> 
+        //     </div>
 
-            <div style={{paddingTop:'1vh',fontSize:'3vh',position:'absolute', top:'-6vh', color:'rgba(255,255,255,1)' }}>
-              Counter Party
-            </div>
-            <div style={{paddingTop:'1vh',fontSize:'3vh',position:'absolute', textAlign:'center', bottom:'-8vh', color:'rgba(255,255,255,1)' }}>
-              Scan QR to load this Table ID
-            </div>
+        //     <div style={{paddingTop:'1vh',fontSize:'3vh',position:'absolute', top:'-6vh', color:'rgba(255,255,255,1)' }}>
+        //       Counter Party
+        //     </div>
+        //     <div style={{paddingTop:'1vh',fontSize:'3vh',position:'absolute', textAlign:'center', bottom:'-8vh', color:'rgba(255,255,255,1)' }}>
+        //       Scan QR to load this Table ID
+        //     </div>
 
-            </div>
-        </div>:
+        //     </div>
+        // </div>
+        :
         <>
           <div onClick={()=>{setdisplayUserErc20Assets(!displayUserErc20Assets)}} className="addAssetButton" style={{position:'absolute', top:'10vh',transform:'scale(3)', right:'5%'}}>
           <AddBoxIcon />
